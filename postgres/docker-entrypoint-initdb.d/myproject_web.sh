5ae0e3a1b3b973106bfc494d726af29730fca77d
lowandrew <andrew.low@canada.ca>
2019-02-08 13:40:30 -0500

    What should be final changes to make CI work

--- postgres/docker-entrypoint-initdb.d/myproject_web.sh
+++ postgres/docker-entrypoint-initdb.d/myproject_web.sh
@@ -1,5 +1,4 @@
-#!/bin/env bash
+#!/bin/bash
 psql -U postgres -c "CREATE USER $DB_USER PASSWORD '$DB_PASS'"
 psql -U postgres -c "ALTER ROLE $DB_USER SUPERUSER"
 psql -U postgres -c "CREATE DATABASE $DB_NAME OWNER $DB_USER"
-psql -U postgres -c "CREATE ROLE $DB_USER"
