47d96cebaaf42e1b8ff55afe525c75f23a941732
Brenna <brenna.arbour@gmail.com>
2019-04-02 17:14:48 -0400

    templates

--- olc_webportalv2/geneseekr/admin.py
+++ olc_webportalv2/geneseekr/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from olc_webportalv2.geneseekr.models import GeneSeekrRequest, GeneSeekrDetail, ParsnpAzureRequest, ParsnpTree
+from olc_webportalv2.geneseekr.models import GeneSeekrRequest, GeneSeekrDetail, ParsnpAzureRequest, ParsnpTree, AMRSummary, AMRAzureRequest
 
 # Register your models here.
 admin.site.register(GeneSeekrRequest)
 admin.site.register(GeneSeekrDetail)
 admin.site.register(ParsnpAzureRequest)
 admin.site.register(ParsnpTree)
+admin.site.register(AMRSummary)
+admin.site.register(AMRAzureRequest)
+
