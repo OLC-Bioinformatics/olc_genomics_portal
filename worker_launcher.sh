e7e782cdbb696cefe8d1e7b4cb6e7e3380ed99fc
lowandrew <andrew.low@canada.ca>
2019-04-05 15:09:32 -0400

    Limit number of workers - temporary solution for task monitoring

--- worker_launcher.sh
+++ worker_launcher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-celery worker -E -l info -A olc_webportalv2.taskapp -n workerD -Q default -c 2 &
+celery worker -E -l info -A olc_webportalv2.taskapp -n workerD -Q default -c 1 &
 celery worker -E -l info -A olc_webportalv2.taskapp -n workerC -Q cowbat -c 2 &
-celery worker -E -l info -A olc_webportalv2.taskapp -n workerG -Q geneseekr -c 2
+celery worker -E -l info -A olc_webportalv2.taskapp -n workerG -Q geneseekr -c 1
