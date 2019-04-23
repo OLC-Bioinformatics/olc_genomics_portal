aa3918e41307704293e5e0e8120113358f4a939b
lowandrew <andrew.low@canada.ca>
2019-04-23 11:40:36 -0400

    Add species/serotype/mlst/rmlst to metadata models - now need to add that data to DB, and allow users to use that data to perform more powerful searches

--- olc_webportalv2/metadata/models.py
+++ olc_webportalv2/metadata/models.py
@@ -8,20 +8,27 @@ quality_choices = (
     ('Reference', 'Reference')
 )
 
+
 class LabID(models.Model):
     labid = models.CharField(max_length=24)
 
     def __str__(self):
         return self.labid
 
+
 class SequenceData(models.Model):
     seqid = models.CharField(max_length=24)
     quality = models.CharField(choices=quality_choices, max_length=128)
     genus = models.CharField(max_length=48)
+    species = models.CharField(max_length=48, blank=True)
+    serotype = models.CharField(max_length=48, blank=True)
+    mlst = models.CharField(max_length=12, blank=True)  # MLST is numeric, but categorical, so keep as CharField
+    rmlst = models.CharField(max_length=12, blank=True)  # Same as MLST. Numeric, but categorical.
     labid = models.ForeignKey(LabID, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.seqid
+
 
 class MetaDataRequest(models.Model):
     seqids = ArrayField(models.CharField(max_length=24), blank=True, default=[])
