3aa3a0049ad962b270da3445f5c2f255744ca720
lowandrew <andrew.low@canada.ca>
2019-04-25 16:07:21 -0400

    First selenium metadata integration test should actually work now. Need to find a better way to select things than by xpath

--- olc_webportalv2/metadata/tests/test_integration.py
+++ olc_webportalv2/metadata/tests/test_integration.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from django.test import LiveServerTestCase
 from django.urls import reverse_lazy
 from olc_webportalv2.users.models import User
-from olc_webportalv2.metadata.models import SequenceData
+from olc_webportalv2.metadata.models import SequenceData, Genus
 
 # TODO: This has made me realize that sometimes I have buttons as links that look like buttons, and
  # sometimes I have them as actual buttons. Should standardize to one or the other.
@@ -15,6 +16,7 @@ class MetadataIntegrationTest(LiveServerTestCase):
         user = User.objects.create(username='testuser', password='password', email='test@test.com')
         user.set_password('password')
         user.save()
+        Genus.objects.create(genus='Escherichia')
         SequenceData.objects.create(seqid='1234-SEQ-0001', genus='Escherichia', quality='Pass')
         SequenceData.objects.create(seqid='1234-SEQ-0002', genus='Escherichia', quality='Reference')
         SequenceData.objects.create(seqid='1234-SEQ-0003', genus='Escherichia', quality='Fail')
@@ -34,9 +36,15 @@ class MetadataIntegrationTest(LiveServerTestCase):
         self.login()
         # This takes us to home page - navigate to metadata page
         self.driver.find_element_by_link_text('Explore').click()
+        # Autocomplete forms make life REALLY DIFFICULT.
+        # Seems to be necessary to: select the little arrow on field of interest by xpath (likely to be very brittle),
+        # then create an giant action chain to actually get something into it.
+        little_clicky_arrow = self.driver.find_element_by_xpath('/html/body/div[2]/form/p[1]/span/span[1]/span/span[2]')
+        ActionChains(self.driver).move_to_element(little_clicky_arrow).click().send_keys('Escherichia').send_keys(Keys.ENTER).send_keys(Keys.ENTER).perform()
+
         # Get Escherichia sequences - default is to do pass and reference quality.
-        self.driver.find_element_by_id('id_genus').send_keys('Escherichia')
-        self.driver.find_element_by_id('id_genus').send_keys(Keys.ENTER)
+        # self.driver.find_element_by_id('id_genus').send_keys('Escherichia')
+        # self.driver.find_element_by_id('id_genus').send_keys(Keys.ENTER)
         self.driver.find_element_by_xpath('//button[text()="Get SeqIDs"]').click()
 
         # Make sure we got two results (3 including table header)
