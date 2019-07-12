f79b4bf18266b35e1bb515bf79f6786572bb23c3
lowandrew <andrew.low@canada.ca>
2019-07-12 15:16:42 -0400

    Make API responses a touch more elegant

--- olc_webportalv2/api/views.py
+++ olc_webportalv2/api/views.py
@@ -1,9 +1,9 @@
 from rest_framework import permissions, views, parsers, response, generics
 from azure.storage.blob import BlockBlobService, BlobPermissions
-from olc_webportalv2.cowbat.models import SequencingRun
+from olc_webportalv2.cowbat.models import SequencingRun, DataFile
 from olc_webportalv2.cowbat.tasks import run_cowbat_batch
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 import os
 
 
@@ -19,6 +19,41 @@ class UploadView(views.APIView):
         if not SequencingRun.objects.filter(run_name=run_name).exists():
             sequencing_run = SequencingRun.objects.create(run_name=run_name)
             sequencing_run.save()
+        sequencing_run = SequencingRun.objects.get(run_name=run_name)
+        # SampleSheet has data we need - read through it.
+        if file_name == 'SampleSheet.csv':
+            instance = DataFile(sequencing_run=sequencing_run,
+                                data_file=file_obj)
+            instance.save()
+            with open('olc_webportalv2/media/{run_name}/SampleSheet.csv'.format(run_name=str(sequencing_run))) as f:
+                lines = f.readlines()
+            seqid_start = False
+            seqid_list = list()
+            realtime_dict = dict()
+            # Sample plate column in SampleSheet should have Lab/Whatever other ID.
+            # Store that data in a dictionary with SeqIDs as keys and LabIDs as values
+            sample_plate_dict = dict()
+            for i in range(len(lines)):
+                if seqid_start:
+                    seqid = lines[i].split(',')[0]
+                    labid = lines[i].split(',')[2]
+                    sample_plate_dict[seqid] = labid
+                    try:
+                        realtime = lines[i].rstrip().split(',')[9]
+                    except IndexError:
+                        realtime = ''
+                    seqid_list.append(seqid)
+                    if realtime == 'TRUE' or realtime == 'VRAI':
+                        realtime_dict[seqid] = 'True'  # Not sure JSONField this gets stored in can handle bool
+                    else:
+                        realtime_dict[seqid] = 'False'
+                if 'Sample_ID' in lines[i]:
+                    seqid_start = True
+            SequencingRun.objects.filter(pk=sequencing_run.pk).update(seqids=seqid_list,
+                                                                      realtime_strains=realtime_dict,
+                                                                      sample_plate=sample_plate_dict)
+
+        # InterOp files will always have .bin extension, and need to be put into the InterOp folder
         if file_name.endswith('.bin'):
             blob_file_name = os.path.join('InterOp', file_name)
         else:
@@ -43,13 +78,18 @@ class StartCowbatView(generics.RetrieveAPIView):
         return sequencing_run
 
     def retrieve(self, request, *args, **kwargs):
-        sequencing_run = self.get_queryset()
+        try:
+            sequencing_run = self.get_queryset()
+        except SequencingRun.DoesNotExist:
+            raise Http404
         if sequencing_run.status == 'Unprocessed':
             run_cowbat_batch.apply_async(queue='cowbat', args=(sequencing_run.pk, ))
             sequencing_run.status = 'Processing'
             sequencing_run.save()
             return JsonResponse({'status': 'Started assembly of run {}'.format(self.kwargs['run_name'])})
         else:
-            return JsonResponse({'status': 'Did not start assembly for {}'.format(self.kwargs['run_name'])})
+            return JsonResponse({'status': 'Did not start assembly for {run_name}. Status for {run_name} is '
+                                           '{status}'.format(run_name=self.kwargs['run_name'],
+                                                             status=sequencing_run.status)})
 
 
