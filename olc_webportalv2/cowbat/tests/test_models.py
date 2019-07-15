dc5b77716951ad5c8ffbc6afacecc448a3b37e52
lowandrew <andrew.low@canada.ca>
2019-07-15 14:50:39 -0400

    Fix cowbat models test

--- olc_webportalv2/cowbat/tests/test_models.py
+++ olc_webportalv2/cowbat/tests/test_models.py
@@ -3,9 +3,8 @@ from olc_webportalv2.cowbat.models import SequencingRun
 
 
 class SampleTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
+    def setUp(cls):
         run_one = SequencingRun.objects.create(run_name='123456_TEST')
 
     def test_correct_str_method(self):
-        self.assertEqual(str(SequencingRun.objects.get(pk=1)), '123456_TEST')
+        self.assertEqual(str(SequencingRun.objects.get(run_name='123456_TEST')), '123456_TEST')
