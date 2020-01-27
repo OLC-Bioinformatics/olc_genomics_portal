108a83e3288d3cacec7fe1bc6cfcfb8fae1423f3
b-arbour <brenna.arbour@gmail.com>
2020-01-27 11:00:58 -0500

    PrimerFile must only be fasta files

--- olc_webportalv2/primer/forms.py
+++ olc_webportalv2/primer/forms.py
@@ -1,6 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
 from django.forms.formsets import BaseFormSet
-from django.forms import ModelForm
+from django.forms import ModelForm, FileInput
 from django import forms
 
 from .models import PrimerVal
@@ -9,15 +9,16 @@ class PrimerForm(ModelForm):
 
    class Meta:
         model = PrimerVal
-        fields = ['name','path', 'sequence_path', 'primer_file', 'mismatches', 'analysistype']
+        fields = ['name','analysistype','mismatches', 'primer_file', 'ampliconsize' , 'export_amplicons']
         labels = {
             'name': _('Name'),
-            'path': _('Path'),
-            'sequence_path': _('Sequence Path'),
-            'primer_file': _('Primer File'),
-            'mismatches': _('Mismatches'),
-            'kmer_length': _('Subunit'),
-            'cpus': _('CPUs'),
             'analysistype': _('Analysis Type'),
+            'mismatches': _('Mismatches'),
+            'primer_file': _('Primer File'),
+            'ampliconsize': _('Amplicon Size'),         
+            'export_amplicons': _('Export Amplicons'),
             }
-
+            # Limits FileInput to only be .fasta files
+        widgets = {
+            'primer_file':FileInput(attrs={'accept': ".fasta"}),
+            }
