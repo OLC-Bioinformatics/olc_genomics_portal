b6e43dec3095b6d282b4863f5c90bc5d310c729f
b-arbour <brenna.arbour@gmail.com>
2020-01-31 09:21:11 -0500

    Primer now on admin site

--- olc_webportalv2/primer/admin.py
+++ olc_webportalv2/primer/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import PrimerAzureRequest, PrimerVal
 
-# Register your models here.
+admin.site.register(PrimerAzureRequest)
+admin.site.register(PrimerVal)
+
