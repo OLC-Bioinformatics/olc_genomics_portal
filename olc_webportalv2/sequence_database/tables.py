bbc5225573f9c4d2dcab0316fb1a115b89180174
adamkoziol <adam.koziol@inspection.gc.ca>
2024-04-30 19:55:03 +0000

    feat: update tables.py in sequence_database
    This commit updates tables.py in the sequence_database module. It adds new columns 'seqid' and 'cfiaid' to the SequenceDataTable class and updates the Meta class with new attributes and fields.

--- olc_webportalv2/sequence_database/tables.py
+++ olc_webportalv2/sequence_database/tables.py
@@ -1,7 +1,20 @@
 import django_tables2 as tables
 from olc_webportalv2.sequence_database.models import SequenceData
 
+
 class SequenceDataTable(tables.Table):
+
+    seqid = tables.Column(attrs={"td": {"nowrap": "nowrap"}},
+                          order_by='seqid')
+    cfiaid = tables.Column(attrs={"td": {"nowrap": "nowrap"}})
+
     class Meta:
         model = SequenceData
-        attrs = {"class": "table"}
+        # orderable = False
+        attrs = {"class": "sequencetable",
+                 "display": "inline-block",
+                 "position": "relative",
+                 "overflow": "auto",
+                 "style": "overflow-y:scroll"}
+        fields = ('seqid', 'cfiaid', 'rmlst', 'mlst', 'mlst_cc', 'genus', 'species', 'serovar', 'geneseekr', 'vtyper',
+                  'version', 'typing_date')
