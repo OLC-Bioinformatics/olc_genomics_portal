7059bae956a994b29892e097f1af4095a9f7e4bd
adamkoziol <adam.koziol@inspection.gc.ca>
2024-04-30 19:24:57 +0000

    feat: register new models in admin
    This commit registers the ResearchRun, SummaryMetadata, and AzureTask models in the Django admin interface.

--- olc_webportalv2/cowbat/admin.py
+++ olc_webportalv2/cowbat/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import DataFile, SequencingRun, InterOpFile
+from .models import DataFile, SequencingRun, InterOpFile, ResearchRun, SummaryMetadata, AzureTask
 
 
 # Register your models here.
 admin.site.register(DataFile)
 admin.site.register(SequencingRun)
 admin.site.register(InterOpFile)
+admin.site.register(ResearchRun)
+admin.site.register(SummaryMetadata)
+admin.site.register(AzureTask)
