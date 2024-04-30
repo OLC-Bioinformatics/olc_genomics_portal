1d2d13f785a59bc932a3c2e6164eae06aac10224
adamkoziol <adam.koziol@inspection.gc.ca>
2024-04-30 19:45:20 +0000

    feat: update urls.py in geneseekr
    This commit updates urls.py in the geneseekr module. It refactors the URL pattern for geneseekr_processing to improve readability and adds a new URL pattern for genus_autocompleter.

--- olc_webportalv2/geneseekr/urls.py
+++ olc_webportalv2/geneseekr/urls.py
@@ -6,10 +6,13 @@ urlpatterns = [
     # GeneSeekr Stuff
     url(_(r'^geneseekr_home/'), views.geneseekr_home, name='geneseekr_home'),
     url(_(r'^geneseekr_query/'), views.geneseekr_query, name='geneseekr_query'),
-    url(_(r'^geneseekr_processing/(?P<geneseekr_request_pk>\d+)/$'), views.geneseekr_processing, name='geneseekr_processing'),
+    url(_(r'^geneseekr_processing/(?P<geneseekr_request_pk>\d+)/$'), views.geneseekr_processing,
+        name='geneseekr_processing'),
     url(_(r'^geneseekr_results/(?P<geneseekr_request_pk>\d+)/$'), views.geneseekr_results, name='geneseekr_results'),
     url(_(r'^geneseekr_name/(?P<geneseekr_request_pk>\d+)/$'), views.geneseekr_name, name='geneseekr_name'),
-
+    url(_(r'^genus_autocompleter/$'),
+        views.GenusAutoCompleter.as_view(),
+        name='genus_autocompleter'),
     # Tree Stuff
     url(_(r'^tree_home/'), views.tree_home, name='tree_home'),
     url(_(r'^tree_request/'), views.tree_request, name='tree_request'),
