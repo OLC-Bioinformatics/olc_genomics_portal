dba8dafcced6c9d685cb9ded9ddd2c1b8304435d
adamkoziol <adam.koziol@inspection.gc.ca>
2024-05-01 14:00:04 +0000

    refactor: change import of python_2_unicode_compatible
    Due to Django version upgrade, python_2_unicode_compatible was removed. Replaced with version from six library for Python 2 compatibility.

--- olc_webportalv2/users/models.py
+++ olc_webportalv2/users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from config.settings import prod as settings
 
