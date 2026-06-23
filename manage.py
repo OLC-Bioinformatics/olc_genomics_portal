588ef8d546c6fbce9affe470aa8ebafcdbe47d6e
adamkoziol <adam.koziol@inspection.gc.ca>
2026-06-23 19:06:15 +0000

    chore: code cleanup

--- manage.py
+++ manage.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+
+"""
+Django's command-line utility for administrative tasks.
+"""
+
 import os
 import sys
 
@@ -6,20 +11,12 @@ if __name__ == '__main__':
     os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.prod'
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django  # noqa
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
-    import environ
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
 
     # This allows easy placement of apps within the interior
     # olc_webportalv2 directory.
