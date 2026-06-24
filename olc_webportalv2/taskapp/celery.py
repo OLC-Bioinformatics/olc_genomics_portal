f8eee5eedc366a9530859aeadec1a996367e804e
adamkoziol <adam.koziol@inspection.gc.ca>
2026-06-24 19:43:09 +0000

    feat: add workaround to allow python manage.py shell to not autodiscover tasks

--- olc_webportalv2/taskapp/celery.py
+++ olc_webportalv2/taskapp/celery.py
@@ -1,4 +1,10 @@
+"""
+Celery configuration for the Django app.
+"""
+# Local imports
 import os
+
+# Third-party imports
 from celery import Celery
 from django.apps import apps, AppConfig
 from django.conf import settings
@@ -6,23 +12,46 @@ from django.conf import settings
 
 if not settings.configured:
     # set the default Django settings module for the 'celery' program.
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')  # pragma: no cover
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
 
-
+# Create a Celery instance with the name of the Django app and the Redis
+# broker URL.
 app = Celery('olc_webportalv2', broker='redis://redis:6379')
 
 
 class CeleryConfig(AppConfig):
+    """
+    Celery configuration class for the Django app. This class is responsible
+    for setting up Celery with the Django settings and automatically
+    discovering tasks from all installed apps.
+    """
+    # The name of the Django app. This should match the name of the app
     name = 'olc_webportalv2.taskapp'
     verbose_name = 'Celery Config'
 
     def ready(self):
+        """
+        This method is called when the Django app is ready. It configures Celery
+        to use the Django settings and automatically discovers tasks from all
+        installed apps.
+        """
+        # Workaround for the issue where Celery autodiscover_tasks will crash
+        # python manage.py shell due to runtime errors
+        if os.environ.get("SKIP_CELERY_AUTODISCOVER") == "1":
+            return
+
         # Using a string here means the worker will not have to
         # pickle the object when using Windows.
         app.config_from_object('config.settings.prod')
         installed_apps = [app_config.name for app_config in apps.get_app_configs()]
         app.autodiscover_tasks(lambda: installed_apps, force=True)
 
+
 @app.task(bind=True)
 def debug_task(self):
-    print('Request: {0!r}'.format(self.request))  # pragma: no cover
+    """
+    A simple Celery task for debugging purposes. This task prints the request
+    information to the console. It can be used to verify that Celery is set up
+    correctly and that tasks are being executed as expected.
+    """
+    print(f'Request: {self.request!r}')
